ed80e1f42822e02f2138f0f6be9468eb33060921
Miraj <mirajiik@yandex.ru>
2023-05-25 20:13:12 +0500

    Добавьте файлы проекта.

---
+++ PostgreSQL_ENC/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PostgreSQL_ENC.ViewModels.Base;
+
+namespace PostgreSQL_ENC.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "Students";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
---
+++ PostgreSQL_ENC/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PostgreSQL_ENC
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
